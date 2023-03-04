61f17746778e8e6aabafa6a1fab891ae274e6976
RokuFSD <emilianordx@gmail.com>
2023-03-04 15:38:03 -0300

    New messages

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,7 +12,7 @@ export default function Page({currentUser, tickets}) {
   ))
   return (
       <div>
-        <h1> Tickets </h1>
+        <h2> Tickets </h2>
         <table className="table">
           <thead>
           <tr>
