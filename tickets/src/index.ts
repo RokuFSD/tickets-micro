db49506ee70b2703dd216adcd3e55be51c30e89c
RokuFSD <emilianordx@gmail.com>
2023-03-04 16:01:04 -0300

    Added starting message on tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,6 +7,7 @@ import {OrderCreatedListener} from "./events/listeners/order-created-listener";
 import {OrderCancelledListener} from "./events/listeners/order-cancelled-listener";
 
 async function start() {
+  console.log("Starting tickets...")
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
